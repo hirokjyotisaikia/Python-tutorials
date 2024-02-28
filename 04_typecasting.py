8b7c897bde5cf3d75763321839f516cee5563d04
Hirokjyoti Saikia <93503012+hirokjyotisaikia@users.noreply.github.com>
2024-02-28 07:21:17 +0530

    Add files via upload

---
+++ 04_typecasting.py
@@ -0,0 +1,6 @@
+#type casting is just converting the data types if possible
+
+a = 23123
+a = float(a) #convrting the data type to float
+print(type(a)) #printing the data type of a
+print(a+5) #will print a but also add + 5 to the value
